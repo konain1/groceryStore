423e9ba07e59c3d1965172baf51db62d65742dbe
Konain1 <kaunainkasab@gmail.com>
2023-11-11 19:50:57 +0530

    working on cart item adding by js

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const products = [
         id:1,
         title:'Nokia',
         price:'$499',
-        poster:"./car.png",
+        poster:"https://images.unsplash.com/photo-1487260211189-670c54da558d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdoaXRlfGVufDB8fDB8fHww",
         desc:'It same work as factory function work but without defining extra object Here is the code read first explanation after it.',
 
     },
     {
         id:2,
-        title:'Nokia',
-        price:'$499',
-        poster:"./car.png",
+        title:'iphone',
+        price:'$699',
+        poster:"https://images.unsplash.com/photo-1516962215378-7fa2e137ae93?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHdoaXRlfGVufDB8fDB8fHww",
         desc:'It same work as factory function work but without defining extra object Here is the code read first explanation after it.',
 
     },
     {
         id:3,
-        title:'Nokia',
-        price:'$499',
-        poster:"./car.png",
+        title:'samsung',
+        price:'$299',
+        poster:"https://images.unsplash.com/photo-1498335746477-0c73d7353a07?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fHdoaXRlfGVufDB8fDB8fHww",
         desc:'It same work as factory function work but without defining extra object Here is the code read first explanation after it.',
 
     },
@@ -33,6 +33,7 @@ const user = document.querySelector('.user');
 const loginbox = document.querySelector('.loginBox');
 const navbar = document.querySelector('nav');
 const burger = document.querySelector('.burger');
+const product_Cards = document.querySelector('.products-cards')
 
 // Use import statement instead of require
 // import {products }  from './productsData';
@@ -69,3 +70,72 @@ burger.addEventListener('click', () => {
 
     navbar.classList.toggle('active');
 });
+
+function cards(){
+
+    for(let i = 0;i<products.length;i++){
+        product_Cards.innerHTML += `<div class="card" data-id="${products[i].id}" >
+        <div class="cardposter">
+        <img src="${products[i].poster}" alt="">
+        </div>
+        <card class="title"><span>${products[i].title}</span></card>
+        <div class="cardDesc">
+        <p>${products[i].desc}</p>
+        </div>
+        <button onClick="boughtIt(${products[i].id})" class="shopNow-btn"><span class="cardPrice">${products[i].price}</span></button>
+
+        </div>`
+    }
+    
+
+}
+let mapCart = new Map(); 
+
+function boughtIt(pid){
+
+    products.map((item)=>{
+        if(pid ===item.id){
+
+            if(!mapCart.has(pid)){
+                mapCart.set(pid,item)
+            }else{
+                mapCart.delete(pid)
+            }
+            
+        }
+    })
+    
+
+shoppingCart()
+}
+
+function shoppingCart(){
+
+    for (let [key, value] of mapCart) {
+        console.log(key + " is " + value.title);
+        shopping_cart.innerHTML += `
+    <div class="shopping-items">
+    <i class="fa fa-trash"></i>
+    <img
+      src="${value.poster~}"
+      alt=""
+    />
+    <div class="cart-content">
+      <h3>watermelon</h3>
+      <span class="price">$4.99/-</span>
+      <span class="qauntity">Qty : 1</span>
+    </div>
+  </div>
+
+    `;
+    }
+
+
+    
+}
+
+
+
+
+cards();
+
